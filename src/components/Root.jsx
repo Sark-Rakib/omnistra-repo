05cc112e0dcf75adcaf32c4b2d062842118b0704
Rakib Sarker <mdrakibsarkar1@gmail.com>
2026-02-28 01:52:59 +0600

    add code

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from "./Navbar";
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
+import Home from './Home';
+
 
 export default function Root() {
   return (
@@ -9,6 +11,7 @@ export default function Root() {
        <Navbar />
            <Outlet />
            <Footer />
+           <Home></Home>
     </div>
 
   )
